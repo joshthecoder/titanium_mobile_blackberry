eca0c712a527d45021e7e5f28855740011aa8c02
Joshua Roesslein <jroesslein@appcelerator.com>
2012-12-12 12:53:53 -0800

    Implement runScript() zygote extension.

--- runtime/src/zygote.cc
+++ runtime/src/zygote.cc
@@ -4,18 +4,40 @@
 
 using namespace v8;
 
-// print(message)
+// zygote.print([message])
+//
+// Prints a message to stdout and appends a newline.
 static Handle<Value> print(const Arguments& args) {
+  HandleScope handleScope;
+  puts(args.Length() > 0 ? *String::Utf8Value(args[0]) : "");
+  return Undefined();
+}
+
+// zygote.runScript(source)
+//
+// Compile and run the script source in the current
+// context returning the resulting value.
+static Handle<Value> run_script(const Arguments& args) {
   HandleScope handleScope;
 
   if (args.Length() < 1) {
-    Local<Value> e = Exception::Error(
-        String::New("print(message)\n    'message' parameter is required."));
-    return ThrowException(e);
+    return ThrowException(Exception::Error(String::NewSymbol("No source code provided.")));
   }
 
-  printf("%s", *String::Utf8Value(args[0]));
-  return Undefined();
+  // Catch any exceptions thrown while running script.
+  TryCatch try_catch;
+
+  Local<Script> script = Script::Compile(args[0]->ToString());
+  if (script.IsEmpty()) {
+    return try_catch.ReThrow();
+  }
+
+  Local<Value> result = script->Run();
+  if (result.IsEmpty()) {
+    return try_catch.ReThrow();
+  }
+
+  return handleScope.Close(result);
 }
 
 Handle<Object> zygote_create() {
@@ -24,6 +46,7 @@ Handle<Object> zygote_create() {
   Local<Object> zygote = Object::New();
 
   zygote->Set(String::NewSymbol("print"), FunctionTemplate::New(print)->GetFunction());
+  zygote->Set(String::NewSymbol("runScript"), FunctionTemplate::New(run_script)->GetFunction());
 
   return handleScope.Close(zygote);
 }
