886dee18a838efe8bb25f662de6cd7bbc8892b4f
Joshua Roesslein <jroesslein@appcelerator.com>
2012-12-12 12:54:27 -0800

    Begin the very basics of modules.
    This just outlines the very structure of the code
    that will implement the full CommonJS specification.

--- runtime/lib/boot.js
+++ runtime/lib/boot.js
@@ -1,6 +1,46 @@
-// This is the birth place of all JavaScript threads.
-// A zygote object providing the very basics required
-// to bootstrap the system is passed to this entry function.
-(function(zygote) {
-  zygote.print("Hello, world!\n");
+// Zygote.
+//
+// The zygote is a function which is responsible for
+// bootstraping a new JavaScript execution environment.
+// It should be called within a new context before
+// any application scripts are run.
+(function($) {
+  function debug(msg) {
+    // TODO(josh): enable debug via commandline flag.
+    true && $.print(msg);
+  }
+
+
+  // --- Modules ---
+  //
+  // The module system provided is based on the CommonJS specification.
+  // A global function "require" is provided to allow importing
+  // an external module's exported values. Various types of modules
+  // are supported such as JavaScript, native extensions, and JSON.
+
+  function Module(id, parent) {
+    this.id = id;
+    this.parent = parent;
+  }
+
+  // If no relative modules are found for a request
+  // the system will continue by searching each directory
+  // listed in the array of search paths.
+  Module._paths = [ ];
+
+  Module._resolveFilename = function(request, parent) {
+  }
+
+  Module.prototype.require = function(req) {
+    debug('require(' + req + ')');
+  }
+
+  // Setup the root module and the global require.
+  Module._root = new Module('.');
+  require = function(request) { return Module._root.require(request); }
+
+
+  // --- test ---
+
+  require('foo');
 });
